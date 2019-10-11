bf051770ec0fe045848ddddc03cedf7dfaa75057
Mert <mertcan.segmen@hotmail.com>
2019-10-11 03:34:56 +0300

    first commit, add bottom navigation and 3 main fragments

---
+++ app/src/main/java/com/mertcansegmen/locationbasedreminder/MainActivity.java
@@ -0,0 +1,52 @@
+package com.mertcansegmen.locationbasedreminder;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+        
+        initMainFragment();
+    }
+
+    private void initMainFragment() {
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new RemindersFragment()).commit();
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFragment = null;
+
+                    switch (menuItem.getItemId()) {
+                        case R.id.navigation_reminders:
+                            selectedFragment = new RemindersFragment();
+                            break;
+                        case R.id.navigation_notes:
+                            selectedFragment = new NotesFragment();
+                            break;
+                        case R.id.navigation_settings:
+                            selectedFragment = new SettingsFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+                    return true;
+                }
+            };
+}
