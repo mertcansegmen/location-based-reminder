a928e171ae327b620d87bf42c0315fd64ad7496c
Mert <mertcan.segmen@hotmail.com>
2019-10-26 09:57:58 +0300

    make AddEditPlaceFragment implement LocationListener so it can move map to users location when map is displayed

--- app/src/main/java/com/mertcansegmen/locationbasedreminder/ui/addeditplace/AddEditPlaceFragment.java
+++ app/src/main/java/com/mertcansegmen/locationbasedreminder/ui/addeditplace/AddEditPlaceFragment.java
@@ -1,8 +1,15 @@
 package com.mertcansegmen.locationbasedreminder.ui.addeditplace;
 
 
+import android.Manifest;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -21,27 +28,35 @@ import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.mertcansegmen.locationbasedreminder.R;
 import com.mertcansegmen.locationbasedreminder.model.Place;
 import com.mertcansegmen.locationbasedreminder.ui.MainActivity;
 
-public class AddEditPlaceFragment extends Fragment implements OnMapReadyCallback {
+public class AddEditPlaceFragment extends Fragment implements OnMapReadyCallback, LocationListener {
 
-    public static final String PLACE_BUNDLE_KEY ="com.mertcansegmen.locationbasedreminder.EXTRA_PLACE";
+    public static final String PLACE_BUNDLE_KEY = "com.mertcansegmen.locationbasedreminder.EXTRA_PLACE";
     private static final String MAPVIEW_BUNDLE_KEY = "MapViewBundleKey";
-    private static final float DEFAULT_ZOOM = 15;
+    private static final float DEFAULT_ZOOM = 15F;
 
     private Place currentPlace;
 
     private MapView mapView;
     private GoogleMap googleMap;
+    private FusedLocationProviderClient fusedLocationClient;
+    private LocationManager locationManager;
+    private Circle radiusCircle;
 
     private AddEditPlaceFragmentViewModel viewModel;
 
@@ -53,6 +68,7 @@ public class AddEditPlaceFragment extends Fragment implements OnMapReadyCallback
         mapView = view.findViewById(R.id.map_view);
 
         viewModel = ViewModelProviders.of(this).get(AddEditPlaceFragmentViewModel.class);
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext());
 
         if(isGoogleServicesAvailable()) {
             initMap(savedInstanceState);
@@ -71,24 +87,6 @@ public class AddEditPlaceFragment extends Fragment implements OnMapReadyCallback
             currentPlace = getArguments().getParcelable(PLACE_BUNDLE_KEY);
             ((MainActivity)requireActivity()).getSupportActionBar().setTitle(currentPlace.getName());
         }
-    }
-
-    @Override
-    public void onResume() {
-        mapView.onResume();
-        super.onResume();
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        mapView.onDestroy();
-    }
-
-    @Override
-    public void onLowMemory() {
-        super.onLowMemory();
-        mapView.onLowMemory();
     }
 
     private void initMap(Bundle savedInstanceState) {
@@ -120,15 +118,28 @@ public class AddEditPlaceFragment extends Fragment implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         this.googleMap = googleMap;
         googleMap.setMyLocationEnabled(true);
+        configureLocationListener();
         if(currentPlace != null){
-            goToLocation(currentPlace);
+            goToLocation(currentPlace.getLatitude(), currentPlace.getLongitude());
         }
     }
 
-    private void goToLocation(Place place) {
-        LatLng ll = new LatLng(place.getLatitude(), place.getLongitude());
-        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(ll, DEFAULT_ZOOM);
+    private void goToLocation(double lat, double lng) {
+        LatLng latLng = new LatLng(lat, lng);
+        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(latLng, DEFAULT_ZOOM);
         googleMap.moveCamera(update);
+    }
+
+    private void configureLocationListener() {
+        locationManager = (LocationManager) requireActivity().getSystemService(Context.LOCATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (requireActivity().checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                    requireActivity().checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(requireContext(), R.string.grant_location_permission, Toast.LENGTH_SHORT).show();
+            }
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);
     }
 
     @Override
@@ -189,5 +200,48 @@ public class AddEditPlaceFragment extends Fragment implements OnMapReadyCallback
         dialog.setCancelable(false);
         dialog.setArguments(bundle);
         dialog.show(requireActivity().getSupportFragmentManager(), "Name Place");
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if(location != null && currentPlace == null) {
+            LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+            CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, DEFAULT_ZOOM);
+            googleMap.animateCamera(cameraUpdate);
+            locationManager.removeUpdates(this);
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    @Override
+    public void onResume() {
+        mapView.onResume();
+        super.onResume();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
     }
 }
