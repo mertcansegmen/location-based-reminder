4b9762f158b37b3bf2dc8f4c7fc6891f17d8fc07
Mert <mertcan.segmen@hotmail.com>
2019-12-12 03:50:56 +0300

    add associative entity(cross reference table) for linking place and place groups, a many to many relationship

---
+++ app/src/main/java/com/mertcansegmen/locationbasedreminder/model/PlaceGroupPlaceCrossRef.java
@@ -0,0 +1,32 @@
+package com.mertcansegmen.locationbasedreminder.model;
+
+import androidx.room.Entity;
+
+@Entity(primaryKeys = {"placeId", "placeGroupId"})
+public class PlaceGroupPlaceCrossRef {
+
+    private long placeId;
+
+    private long placeGroupId;
+
+    public PlaceGroupPlaceCrossRef(long placeId, long placeGroupId) {
+        this.placeId = placeId;
+        this.placeGroupId = placeGroupId;
+    }
+
+    public long getPlaceId() {
+        return placeId;
+    }
+
+    public void setPlaceId(long placeId) {
+        this.placeId = placeId;
+    }
+
+    public long getPlaceGroupId() {
+        return placeGroupId;
+    }
+
+    public void setPlaceGroupId(long placeGroupId) {
+        this.placeGroupId = placeGroupId;
+    }
+}
