30cdd9bfa28bdfbf5ff870752558f30f64ead471
Mert <mertcan.segmen@hotmail.com>
2019-12-17 23:20:02 +0300

    add viewmodel for add edit place group fragment

---
+++ app/src/main/java/com/mertcansegmen/locationbasedreminder/ui/addeditplacegroup/AddEditPlaceGroupFragmentViewModel.java
@@ -0,0 +1,31 @@
+package com.mertcansegmen.locationbasedreminder.ui.addeditplacegroup;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+import com.mertcansegmen.locationbasedreminder.model.PlaceGroupWithPlaces;
+import com.mertcansegmen.locationbasedreminder.repository.PlaceGroupRepository;
+
+public class AddEditPlaceGroupFragmentViewModel extends AndroidViewModel {
+
+    private PlaceGroupRepository repository;
+
+    public AddEditPlaceGroupFragmentViewModel(@NonNull Application application) {
+        super(application);
+        repository = new PlaceGroupRepository(application);
+    }
+
+    public void insert(PlaceGroupWithPlaces placeGroupWithPlaces) {
+        repository.insertPlaceGroupWithPlaces(placeGroupWithPlaces);
+    }
+
+    public void update(PlaceGroupWithPlaces placeGroupWithPlaces) {
+        repository.updatePlaceGroupWithPlaces(placeGroupWithPlaces);
+    }
+
+    public void delete(PlaceGroupWithPlaces placeGroupWithPlaces) {
+        repository.deletePlaceGroupWithPlaces(placeGroupWithPlaces);
+    }
+}
