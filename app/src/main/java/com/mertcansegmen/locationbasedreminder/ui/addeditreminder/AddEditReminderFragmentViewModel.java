da7bc7cc23b8ee47e55aacaf03e7659699d18716
Mert <mertcan.segmen@hotmail.com>
2020-01-05 20:52:25 +0300

    add AddEditReminderFragmentViewModel and Selectable marker interface

---
+++ app/src/main/java/com/mertcansegmen/locationbasedreminder/ui/addeditreminder/AddEditReminderFragmentViewModel.java
@@ -0,0 +1,58 @@
+package com.mertcansegmen.locationbasedreminder.ui.addeditreminder;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.mertcansegmen.locationbasedreminder.model.Place;
+import com.mertcansegmen.locationbasedreminder.model.PlaceGroupWithPlaces;
+import com.mertcansegmen.locationbasedreminder.model.Reminder;
+import com.mertcansegmen.locationbasedreminder.model.ReminderWithNotePlacePlaceGroup;
+import com.mertcansegmen.locationbasedreminder.repository.PlaceGroupRepository;
+import com.mertcansegmen.locationbasedreminder.repository.PlaceRepository;
+import com.mertcansegmen.locationbasedreminder.repository.ReminderRepository;
+
+import java.util.List;
+
+public class AddEditReminderFragmentViewModel extends AndroidViewModel {
+
+    private ReminderRepository reminderRepository;
+    private PlaceGroupRepository placeGroupRepository;
+    private PlaceRepository placeRepository;
+
+    private MutableLiveData<Selectable> selected = new MutableLiveData<>();
+    private LiveData<List<Place>> allPlaces;
+    private LiveData<List<PlaceGroupWithPlaces>> allPlaceGroups;
+
+    public AddEditReminderFragmentViewModel(@NonNull Application application) {
+        super(application);
+        reminderRepository = new ReminderRepository(application);
+        placeGroupRepository = new PlaceGroupRepository(application);
+        placeRepository = new PlaceRepository(application);
+        allPlaces = placeRepository.getAllPlaces();
+        allPlaceGroups = placeGroupRepository.getAllPlaceGroupsWithPlaces();
+    }
+
+    public void insert(ReminderWithNotePlacePlaceGroup reminder) {
+        reminderRepository.insert(reminder);
+    }
+
+    public void update(ReminderWithNotePlacePlaceGroup reminder) {
+        reminderRepository.update(reminder);
+    }
+
+    public void delete(ReminderWithNotePlacePlaceGroup reminder) {
+        reminderRepository.delete(reminder);
+    }
+
+    public void select(Selectable selectable) {
+        selected.setValue(selectable);
+    }
+
+    public LiveData<Selectable> getSelected() {
+        return selected;
+    }
+}
---
+++ app/src/main/java/com/mertcansegmen/locationbasedreminder/ui/addeditreminder/Selectable.java
@@ -0,0 +1,6 @@
+package com.mertcansegmen.locationbasedreminder.ui.addeditreminder;
+
+/**
+ * Marker interface which represents objects that can be selected as a location for a reminder.
+ */
+public interface Selectable {}
